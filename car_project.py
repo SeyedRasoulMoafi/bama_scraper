3692e53a8f19721b7514a34b7c63d5fc3f71f2e1
rasoul <rasoulhk8@gmail.com>
2022-11-17 14:40:32 +0330

    First_Commit

---
+++ car_project.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+
+
+###connecting to DB##
+
+connection_args = {
+    'host': 'localhost',
+    'database': 'khodro',
+    'user': 'root',
+    'password': 'myHK58845884hk',
+    'port' : '3308'
+}
+
+cnx = mysql.connector.connect(**connection_args)
+
+mycursor = cnx.cursor()
+
+#### geting information from website###
+
+r = requests.get(r'https://bama.ir/car/renault-tondar90?priced=1')
+soup = BeautifulSoup(r.text,'html')
+val = soup.find_all('a', attrs ={'title':'رنو، تندر 90'})
+
+for i, link in enumerate(soup.find_all('a', attrs ={'title':'رنو، تندر 90'})):
+
+###cars model
+
+    model = val[i].find('div',attrs = {'class':'bama-ad__detail-holder'})
+    paragraphs = str(model)
+    Model = int(re.findall(r'>(\d{4}).*?/span>',paragraphs)[0])
+
+###cars price
+
+    price = val[i].find('div',attrs = {'class':'bama-ad__price-holder'})
+    paragraphs = str(price)
+    Price = re.findall(r'>(\d.*?)</span>',paragraphs)[0]
+
+### Date
+
+    x = datetime.datetime.now()  
+    ol = x.strftime('%Y-%m-%d %H:%M:%S')
+    date_time_obj = datetime.datetime.strptime(ol, '%Y-%m-%d %H:%M:%S')
+   
+    mycursor.execute("INSERT INTO car VALUES (\'L90\', %s, %s , %s, DEFAULT)", [Model,Price, date_time_obj])
+
+    cnx.commit()
